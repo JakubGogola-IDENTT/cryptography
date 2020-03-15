4d1f7ead1ea69757153446e910c4f91a243d9324
JakubGogola <jakubgogola97@gmail.com>
2020-03-15 22:34:22 +0100

    next text

---
+++ list1/test2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <bitset>
+
+const int MAX = 100;
+int r[MAX];
+
+int predict(int idx) {
+    r[idx] = r[idx - 31] + r[idx - 3] % 2147483648;
+
+    return r[idx];
+}
+
+int main() {
+    srand(1);
+    int expected, predicted;
+    int i;
+
+    for (i = 0; i < 30; i++) {
+        r[i] = rand();
+    }
+
+    unsigned int correct = 0;
+
+    // test
+    for (i = 0; i < MAX; i++) {
+        expected = rand();
+        predicted = predict(i);
+
+        std::bitset<32> bits(expected ^ ~predicted);
+
+        correct += bits.count();
+
+        std::cout << ((double) correct) / (i + 1) / 32 << std::endl;
+
+        if (predicted != expected) {
+            r[i] = expected % 2147483648;
+        }
+    }
+}
